fc63b0204e2615898a0806281db592644cd1a072
William A. Sease <WilliamASease@gmail.com>
2024-08-08 00:21:51 -0400

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+import './App.css';
+
+function App() {
+  useEffect(() => {
+    // Fetch the list of sound files from the server
+    fetch('/williamasease.github.io/hl_text2speech_revived/snd')
+      .then((response) => response.json())
+      .then((data) => console.info(data))
+      .catch((error) => console.error('Error fetching sound files:', error));
+  }, []);
+  
+  return (
+    <div className="App">
+      APP
+    </div>
+  );
+}
+
+export default App;
